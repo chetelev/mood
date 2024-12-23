5aa31a08b0c3a8e0c6d65b3b99cc9b06e7ace2f7
chetelev <chetelev@gmail.com>
2024-12-23 17:46:15 +0100

    expose new route for patch journal

---
+++ app/api/journal/[id]/route.ts
@@ -0,0 +1,21 @@
+import { getUserByClerkId } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export const PATCH = async (request, { params }) => {
+  const { content } = await request.json();
+  const user = await getUserByClerkId();
+  const updatedEntry = await prisma.journalEntry.update({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id: params.id,
+      },
+    },
+    data: {
+      content,
+    },
+  });
+
+  return NextResponse.json({ data: updatedEntry });
+};
