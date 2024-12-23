d3ba918f94b4d366dc6bdfabd5614ebb78007ec8
chetelev <chetelev@gmail.com>
2024-12-23 17:45:44 +0100

    react-autosave, patch data on debounce
    use react-autosave lib to help with debounce and patch journal data
    content

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,13 +1,26 @@
 "use client";
 
+import { updatedEntry } from "@/lib/api";
 import { useState } from "react";
+import { useAutosave } from "react-autosave";
 
 const Editor = ({ entry }) => {
   const [value, setValue] = useState(entry.content);
+  const [isLoading, setIsLoading] = useState(false)
+
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsLoading(true)
+      const updated = await updatedEntry(entry.id, _value)
+      setIsLoading(false)
+    }
+  })
 
   return (
     <div>
       <div className="w-full h-full">
+      {isLoading && <div>...loading</div>}
         <textarea
           className="w-full h-full text-xl p-8"
           value={value}
